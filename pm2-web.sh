47c2a803c1102e5c5c2668b75aa4cf03c487b579
timo <support@arkoisystems.com>
2021-04-18 15:12:09 +0200

    Changed some stuff

--- pm2-web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting to setup the webserver.."
-echo "Installing the webserver."
-npm install
-echo "Building the production build."
-npx next build
-echo "Starting the production build."
-npx next start -p 4000
